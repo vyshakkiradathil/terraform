1642acbc97ef73b9674d09df5ad446fe869ffb19
vaishakh k <38469664+vyshakkiradathil@users.noreply.github.com>
2021-07-01 20:46:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.65.0"
+    }
+  }
+}
+
 module "vnet" {
   source  = "Azure/vnet/azurerm"
   version = "2.5.0"
